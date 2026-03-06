25e9019bfed5a0566c00628ccfd28f815f5ff65a
I-Sadeque <ilham_sadeque@outlook.com>
2026-03-06 15:27:26 +0000

    fixing

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -1,18 +1,84 @@
 from datetime import date
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 BELOW
-#
-#####
+class Trainee:
+    """Structure for the main class for trainee"""
+
+    def __init__(self, name: str, email: str, dob: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = dob
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """calculates and returns the age of someone"""
+        age = 0
+        age = date.today().year - self.date_of_birth.year - ((date.today().month,
+                                                              date.today().day) < (self.date_of_birth.month, self.date_of_birth.day))
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        """adds a assessment to the assessment list"""
+        if isinstance(assessment, Assessment) == False:
+            raise TypeError("That is not an Assessment")
+        self.assessments.append(assessment)
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        """Looks to see if an assessment is in a list using its name"""
+        for assessments in self.assessments:
+            if assessments.name == name:
+                return assessments
+        return None
+
+    def get_assessment_of_type(self, type: str) -> list[Assessment]:
+        """collects all the assessments that are present in the list"""
+        list_of_a_type = []
+        for assessment in self.assessments:
+            if assessment.type == type:
+                list_of_a_type.append(assessment)
+        return list_of_a_type
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 ABOVE
-#
-#####
+class Assessment:
+    def __init__(self, name: str, type: str, score: int):
+        self.name = name
+        self.type = type
+        self.score = score
+        self.valid_types = ['multiple-choice', 'technical', 'presentation']
+        if self.type.lower() not in self.valid_types:
+            raise ValueError("That type is not a assessment type")
+        if self.score < 0 or self.score > 100:
+            raise ValueError("That percentage is not possible")
+
+
+class MultipleChoiceAssessment(Assessment):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        self.type = 'multiple-choice'
+
+    def calculate_score(self) -> float:
+        return (self.score * 0.7)
+
+
+class TechnicalAssessment(Assessment):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        self.type = 'technical'
+
+    def calculate_score(self) -> float:
+        return self.score
+
+
+class PresentationAssessment(Assessment):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+        self.type = 'presentation'
+
+    def calculate_score(self) -> float:
+        return (self.score * 0.6)
 
 
 if __name__ == "__main__":
